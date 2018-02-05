ad5f3c44cc8157c60629d218ffb6d8a091e367d8
HIKO1021024 <ana@m-hiko.info>
2018-02-06 02:41:33 +0900

    farst

---
+++ del_koukoku/del_koukoku.js
@@ -0,0 +1,10 @@
+//ÉXÇ¡Ç∆åªÇÍÇÈçLçêÇè¡Ç∑
+javascript:var element = $('#geniee_overlay');
+element.remove();
+
+var element = $("[class^='adstir-ad-slot-']");
+element.remove();
+
+
+!function(d,f,s){s=d.createElement("script");s.src="//j.mp/1bPoAXq";s.onload=function(){f(jQuery.noConflict(1))};d.body.appendChild(s)}(document,function($){});
+
